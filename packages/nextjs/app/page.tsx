76091b22042d195887779c2ed90e2e5a265f3f2b
Jayy4rl <onyinwajei@gmail.com>
2025-10-23 22:46:18 +0100

    feat: add Web3 frontend for token and NFT contracts

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -5,6 +5,9 @@ import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Logo } from "~~/components/Logo";
+import { NFTCollection } from "~~/components/example-ui/NFTCollection";
+import { TokenBalance } from "~~/components/example-ui/TokenBalance";
+import { TokenTransfer } from "~~/components/example-ui/TokenTransfer";
 import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
@@ -62,6 +65,21 @@ const Home: NextPage = () => {
               </Link>{" "}
               tab.
             </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+          <div className="flex justify-center items-center gap-6 flex-col sm:flex-row">
+            <div className="flex flex-col px-10 py-10 text-center items-center rounded-3xl">
+              <TokenBalance />
+            </div>
+            <div className="flex flex-col px-10 py-10 text-center items-center rounded-3xl">
+              <TokenTransfer />
+            </div>
+            <div className="flex flex-col px-10 py-10 text-center items-center rounded-3xl">
+              <NFTCollection />
+            </div>
           </div>
         </div>
       </div>
